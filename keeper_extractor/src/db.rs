7f53bb192fa9a37b1703f1fe1a317e98898a2ead
googrand <sharkanovnikita@gmail.com>
2021-05-28 15:40:03 +0600

    read bytes

--- keeper_extractor/src/db.rs
+++ keeper_extractor/src/db.rs
@@ -5,6 +5,13 @@ use serde::{
     Deserialize,
 };
 
+#[derive(Insertable,Serialize,Deserialize)]
+#[table_name = "pollers_data"]
+pub struct Blocks {
+    pub block_id: i32,
+    pub poller_id: i32,
+}
+
 #[derive(Serialize,Deserialize,Queryable)]
 pub struct Data {
     id: i32,
@@ -14,14 +21,14 @@ pub struct Data {
 
 
 impl Blocks {
-    pub async fn check(
+    pub async fn save(
         &self,
         conn: &PgConnection, 
     ) -> Result<bool> {
-        diesel::sql_query("call GiveFee($1,$2)")
-            .bind::<diesel::sql_types::Varchar,_>(self.user_address.clone())
-            .bind::<diesel::sql_types::Integer,_>(self.round_id)
-            .get_result::<Allowance>(conn)
+        diesel::sql_query("INSERT INTO pollers_data (block_id, poller_id) VALUES ($1, $2)")
+            .bind::<diesel::sql_types::Integer,_>(self.block_id)
+            .bind::<diesel::sql_types::Integer,_>(self.poller_id)
+            .get_result::<Data>(conn)
             .map_err(|e|e.into())
             .map(|r|r.res)
     }
@@ -29,9 +36,9 @@ impl Blocks {
     pub async fn get(
         &self,
         conn: &PgConnection, 
-    ) -> Result<Voter> {
+    ) -> Result<Data> {
         pollers_data::table
-            .filter(pollers_data::round_id.eq(self.round_id))
+            .filter(pollers_data::poller_id.eq(self.poller_id))
             .get_result(conn)
             .map_err(|e|e.into())
     }
--- keeper_extractor/src/main.rs
+++ keeper_extractor/src/main.rs
@@ -12,6 +12,10 @@ use diesel::{
     prelude::*,
     r2d2::{ConnectionManager, Pool},
 };
+use super::db::{
+    Blocks,
+    Data,
+};
 
 #[tokio::main]
 async fn main() -> web3::contract::Result<()> {
@@ -27,20 +31,34 @@ async fn main() -> web3::contract::Result<()> {
 
     let http = web3::transports::Http::new("https://rpcapi.fantom.network").expect("err creating http");
     let web3 = web3::Web3::new(http);
-    let current_block = web3.block_number().await;
-    //////
-    let block: u64 = block_number.into(block_number);
-    ////
-    let topics = TopicFilter::default();
-    topics.topic0 = method_hash;
+    loop {
+        // let block_data: Data =  ..........................
+        // let prev_block = block_data.get(&pool);
+        let block = 7979676 as u64;
+        let prev_block = BlockNumber::Number(block);
+        let current_block = web3.block_number().await;
+        let topics = TopicFilter::default();
+        topics.topic0 = method_hash;
 
-    let filter = FilterBuilder::default()
+        let filter = FilterBuilder::default()
                     .from_block(prev_block) // missing
                     .to_block(current_block)
                     .address(balance_keeper)
                     .topic_filter(topics)
                     .build();
-    let result = web3.logs(filter).await;
+        let result = web3.logs(filter).await;
+        for block in result {
+            let bytes = block.data;
+            let user_address = bytes.slice(0, 20);
+            println("Got address {}", user_address)
+            let giver_address = bytes.slice(20, 40);
+            println("Got second address {}", giver_address)
+            let amount = bytes.slice_from(40);
+            println("Got amount {}", amount)
+        }
 
-    // take every data with for cycle
+        delay_for(Duration::from_secs((60*60) as u64)).await;
+    }
+
+
 }
