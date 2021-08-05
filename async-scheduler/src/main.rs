c00fab240989dce0d0b2023743095ea3d91e6a20
Anton Davydov <fetsorn@Antons-MacBook-Air.local>
2021-08-05 21:08:05 +0300

    fix: merge main from master

--- async-scheduler/src/main.rs
+++ async-scheduler/src/main.rs
@@ -1,5 +1,4 @@
-// use tokio_cron_scheduler::{JobScheduler, JobToRun, Job};
-use anyhow::{Context, Result, Error};
+use tokio_cron_scheduler::{JobScheduler, JobToRun, Job};
 
 use async_scheduler::events_extractor::EventsExtractor;
 use async_scheduler::forum_extractor::ForumExtractor;
@@ -10,8 +9,8 @@ use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::PgConnection;
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    // let mut sched = JobScheduler::new();
+async fn main() {
+     let mut sched = JobScheduler::new();
 
     let manager = ConnectionManager::<PgConnection>::new(
         std::env::var("DATABASE_URL").expect("missing db url"),
@@ -23,25 +22,24 @@ async fn main() -> Result<()> {
     let p = pool.clone();
     tokio::task::spawn(async move {
         EventsExtractor::new(p.clone()).run().await;
-    })
-    .await?;
+    });
 
-    // let p = pool.clone();
-    // tokio::task::spawn(async move {
-    //     ForumExtractor::new(p.clone()).run().await;
-    // }).await.unwrap();
+    let p = pool.clone();
+    tokio::task::spawn(async move {
+        KeeperExtractor::new(p.clone()).run().await;
+    });
 
-    // let p = pool.clone();
-    // tokio::task::spawn(async move {
-    //     KeeperExtractor::new(p.clone()).run().await;
-    // });
 
-    // sched.add(Job::new("0 0 * * * *", move |_,_| {
-    //     let p = pool.clone();
-    //     tokio::task::spawn(async move {
-    //         CoingeckoPrice::new(p.clone()).run().await;
-    // });}).unwrap()).unwrap();
+    sched.add(Job::new("0 0 * * * *", move |_,_| {
+         let p = pool.clone();
+         tokio::task::spawn(async move {
+             CoingeckoPrice::new(p).run().await;
+         });
+         let p = pool.clone();
+         tokio::task::spawn(async move {
+            ForumExtractor::new(p).run().await;
+         });
+    }).unwrap()).unwrap();
 
-    // sched.start().await.unwrap();
-    Ok(())
+    sched.start().await.unwrap();
 }
