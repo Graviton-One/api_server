0aaf91d17b34b401d2b80f0bb5c53ccd5925e80e
GooGrand <sharkanovnikita@gmail.com>
2021-08-25 20:57:11 +0600

    added txn structure

--- pool_transaction_poller/src/db.rs
+++ pool_transaction_poller/src/db.rs
@@ -1,10 +1,10 @@
 use std::sync::Arc;
-
 use diesel::{
     sql_types::*,
     prelude::*,
     r2d2::{ConnectionManager, Pool},
 };
+use chrono::NaiveDateTime;
 
 #[derive(Default, Debug, Clone, QueryableByName)]
 pub struct SinglePool {
@@ -25,5 +25,25 @@ impl PoolAddresses {
         LEFT JOIN dexes AS d ON d.chain_id = c.id 
         LEFT JOIN pools AS p ON d.id = p.dex_id;").get_results::<PoolAddressess>(&conn.get().unwrap())
         .unwrap()
+    }
+}
+
+pub struct PoolTransaction {
+    id: i64,
+    pool_id: i64,
+    txn_type: String,
+    txn_hash: String,
+    amount: f64,
+    value: f64,
+    timestamp: NaiveDateTime
+    // picture and pool name we will get from pools table
+}
+
+impl PoolTransaction {
+    fn save_txn(&self) -> () {
+        diesel::insert_into(pool_transactions::table)
+        .values(self)
+        .execute(conn)
+        .map_err(|e|e.into());
     }
 }
